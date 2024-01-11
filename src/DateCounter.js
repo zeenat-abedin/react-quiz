6d829902b104bfbe3c774b8f448cfab13892037f
zeenat-abedin <zeenu.23@gmail.com>
2024-01-11 22:12:28 +0530

    Add reset action to DateCounter

--- src/DateCounter.js
+++ src/DateCounter.js
@@ -1,5 +1,7 @@
 import { useReducer } from "react";
 
+const initialState = { count: 0, step: 1 };
+
 function reducer(state, action) {
   console.log(state, action);
 
@@ -12,13 +14,14 @@ function reducer(state, action) {
       return { ...state, count: action.payload };
     case "setStep":
       return { ...state, step: action.payload };
+    case "reset":
+      return initialState;
     default:
       throw new Error("Unknown action");
   }
 }
 
 function DateCounter() {
-  const initialState = { count: 0, step: 1 };
   const [state, dispatch] = useReducer(reducer, initialState);
   const { count, step } = state;
 
@@ -43,8 +46,7 @@ function DateCounter() {
   };
 
   const reset = function () {
-    // setCount(0);
-    // setStep(1);
+    dispatch({ type: "reset" });
   };
 
   return (
