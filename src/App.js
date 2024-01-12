549a313cfda2536a9cc94f5e5eea405b5495a7b0
zeenat-abedin <zeenu.23@gmail.com>
2024-01-12 22:36:26 +0530

    Fix typo in server command and add a data fetching reducer

--- src/App.js
+++ src/App.js
@@ -1,7 +1,34 @@
+import { useEffect, useReducer } from "react";
+
 import Header from "./Header";
 import Main from "./Main";
 
+const initialState = {
+  questions: [],
+
+  //loading, error, active, ready, finished
+  status: "loading",
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "dataReceived":
+      return { ...state, questions: action.payload, status: "ready" };
+    default:
+      throw new Error("Invalid Action Type");
+  }
+}
+
 function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  useEffect(() => {
+    fetch(`http://localhost:8000/questions`)
+      .then((res) => res.json())
+      .then((data) => dispatch({ type: "dataReceived", payload: data }))
+      .catch((err) => console.error("Error"));
+  }, []);
+
   return (
     <div className="app">
       <Header />
