04aee6ba9288f5d3fbebbe1180841f67bffc53f1
zeenat-abedin <zeenu.23@gmail.com>
2024-01-12 21:29:27 +0530

    code cleanup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import Header from "./Header";
+import Main from "./Main";
 
 function App() {
   return (
     <div className="app">
       <Header />
+
+      <Main>
+        <p>1/15</p>
+        <p>Questions</p>
+      </Main>
     </div>
   );
 }
