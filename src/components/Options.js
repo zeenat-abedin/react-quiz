0490941c1cef001ad307a2ca3a9f55065ce8fae3
zeenat-abedin <zeenu.23@gmail.com>
2024-01-13 19:07:32 +0530

    Update Options component to disable buttons after answering

--- src/components/Options.js
+++ src/components/Options.js
@@ -1,15 +1,22 @@
 import React from "react";
 
 export default function Options({ question, dispatch, answer }) {
+  const hasAnswered = answer !== null;
+
   return (
     <div className="options">
       {question.options.map((option, index) => (
         <button
           className={`btn btn-option ${index === answer ? "answer" : ""} ${
-            index === question.correctOption ? "correct" : "wrong"
+            hasAnswered
+              ? index === question.correctOption
+                ? "correct"
+                : "wrong"
+              : ""
           }`}
           key={option}
           onClick={() => dispatch({ type: "newAnswer", payload: index })}
+          disabled={hasAnswered}
         >
           {option}
         </button>
