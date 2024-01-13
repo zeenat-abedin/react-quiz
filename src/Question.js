0289d9c83e039176db3b5feafd6e444754d8906a
zeenat-abedin <zeenu.23@gmail.com>
2024-01-13 13:24:54 +0530

    Add options and buttons to Question component

--- src/Question.js
+++ src/Question.js
@@ -1,9 +1,17 @@
 import React from "react";
 
 export default function Question({ question }) {
+  console.log(question);
   return (
     <div>
       <h4>{question.question}</h4>
+      <div className="options">
+        {question.options.map((option) => (
+          <button className="btn btn-option" key={option}>
+            {option}
+          </button>
+        ))}
+      </div>
     </div>
   );
 }
