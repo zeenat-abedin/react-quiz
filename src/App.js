ccc09c883f26390f2779a18c3f1d496a6d8f9096
zeenat-abedin <zeenu.23@gmail.com>
2024-01-13 12:30:03 +0530

    Add StartScreen component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Header from "./Header";
 import Main from "./Main";
 import Loader from "./Loader";
 import Error from "./Error";
+import StartScreen from "./StartScreen";
 
 const initialState = {
   questions: [],
@@ -40,6 +41,7 @@ function App() {
       <Main>
         {status === "loading" && <Loader />}
         {status === "error" && <Error />}
+        {status === "ready" && <StartScreen />}
       </Main>
     </div>
   );
---
+++ src/StartScreen.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function StartScreen() {
+  return (
+    <div className="">
+      <h2>Welcome to the React Quiz!</h2>
+      <h3>X questions to test your React Mastery</h3>
+      <button onClick={() => {}} id="start-btn" disabled>
+        Let's start
+      </button>
+    </div>
+  );
+}
