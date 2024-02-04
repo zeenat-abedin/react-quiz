17f014926db34f40bf5b37fd95fb2cd1718b67a0
zeenat-abedin <zeenu.23@gmail.com>
2024-02-04 22:42:54 +0530

    Update progress bar and question components

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,11 +1,10 @@
-import React from "react";
-
 import { useQuiz } from "../contexts/QuizContext";
 import Options from "./Options";
 
-export default function Question() {
+function Question() {
   const { questions, index } = useQuiz();
   const question = questions.at(index);
+
   return (
     <div>
       <h4>{question.question}</h4>
@@ -13,3 +12,5 @@ export default function Question() {
     </div>
   );
 }
+
+export default Question;
