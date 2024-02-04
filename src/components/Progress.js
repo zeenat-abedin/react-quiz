078acc7c81adf281515ec3c06b8be40371c0897d
zeenat-abedin <zeenu.23@gmail.com>
2024-02-04 18:37:48 +0530

    Update Progress component to use context

--- src/components/Progress.js
+++ src/components/Progress.js
@@ -1,12 +1,11 @@
 import React from "react";
 
-export default function Progress({
-  index,
-  numOfQuestions,
-  points,
-  maxPossiblePoints,
-  answer,
-}) {
+import { useQuiz } from "../contexts/QuizContext";
+
+export default function Progress() {
+  const { index, numOfQuestions, points, maxPossiblePoints, answer } =
+    useQuiz();
+
   return (
     <header className="progress">
       <progress max={numOfQuestions} value={index + Number(answer !== null)} />
