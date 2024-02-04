3a8f86bd89781e48070c2d7c0d7cbf6a0c15a053
zeenat-abedin <zeenu.23@gmail.com>
2024-02-04 22:38:11 +0530

    Update Timer component to use seconds instead of secs

--- src/components/Timer.js
+++ src/components/Timer.js
@@ -1,25 +1,30 @@
 import { useEffect } from "react";
-
 import { useQuiz } from "../contexts/QuizContext";
 
-export default function Timer() {
-  const { secondsRemaining, dispatch } = useQuiz();
+function Timer() {
+  const { dispatch, secondsRemaining } = useQuiz();
 
   const mins = Math.floor(secondsRemaining / 60);
-  const secs = secondsRemaining % 60;
+  const seconds = secondsRemaining % 60;
 
-  useEffect(() => {
-    const id = setInterval(() => {
-      dispatch({ type: "tick" });
-    }, 1000);
-    return () => clearInterval(id);
-  }, [dispatch]);
+  useEffect(
+    function () {
+      const id = setInterval(function () {
+        dispatch({ type: "tick" });
+      }, 1000);
+
+      return () => clearInterval(id);
+    },
+    [dispatch]
+  );
 
   return (
     <div className="timer">
       {mins < 10 && "0"}
-      {mins} : {secs < 10 && "0"}
-      {secs}
+      {mins}:{seconds < 10 && "0"}
+      {seconds}
     </div>
   );
 }
+
+export default Timer;
